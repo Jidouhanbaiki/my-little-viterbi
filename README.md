4e73246cf43e542f3ed12a01f1c3343d47c2252f
Jidouhanbaiki <ab2akiba@gmail.com>
2016-02-15 11:19:39 +0200

    more refactoring, added docstrings

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ Python 3.5
 * NLTK 3.1
 * Pykov
 * six
+
+
+##List of NLTK tools used:
+* default tokenizer
+* default pos-tagger
+* bigrams() - generates bigrams from a list, nothing fancy though
+* Conditional Frequency Distribution tool ( ConditionalFreqDist() )
+
+##List of Pykov functions used:
+* Matrix() - generates a matrix based on the frequencies of either 
+* Matrix.stochastic() - 
--- pykov_gen2.py
+++ pykov_gen2.py
@@ -2,8 +2,10 @@ import pykov
 import nltk
 import six
 import random
+import sys
 
 FILE_NAME = "orwell.txt"
+
 
 def stochastic(self):  # from pykov, modified
     """
@@ -32,6 +34,11 @@ def stochastic(self):  # from pykov, modified
 
 
 def process_file(file_name):
+    """
+    Process the file with the source text.
+    :param file_name:
+    :return: a list of words
+    """
     f = open(file_name, "r")
     remove_colon = True   # toggle it off to have result with colons
     if remove_colon:
@@ -40,14 +47,14 @@ def process_file(file_name):
         char_buffer = ""
         for char in f.read():
             if char.isalnum() or char == "_":
-                char_buffer+=char.lower()
+                char_buffer += char.lower()
             else:
                 if char != ":":
                     raw += char_buffer
                     raw += char
                 char_buffer = ""
     else:
-        raw = [ char.lower() for char in f.read()]
+        raw = [char.lower() for char in f.read()]
         raw = "".join(raw)
 
     f.close()
@@ -55,116 +62,130 @@ def process_file(file_name):
     # "tokenized_sentences" looks like this: [ [ word, word word ], [word, word] ]
     tokenized_sentences = [nltk.word_tokenize(sent) for sent in nltk.sent_tokenize(raw)]
 
-
-    # adding START and END of sentence tokens
-    # TEMPORARY REMOVED
-    tokenized_lexica = []  #used in order to filter the input later
+    tokenized_lexica = []
     for sent in tokenized_sentences:
-        #tokenized_lexica.append("START")
+        #tokenized_lexica.append("START")  # removed for now for the sake of simplicity
         for token in sent:
             tokenized_lexica.append(token)
-        #tokenized_lexica.append("END")
+        #tokenized_lexica.append("END")  # removed for now for the sake of simplicity
     return tokenized_lexica
 
 
-def generate_word_word_matrix(tokenized_lexica):
-    # creates a list of bigrams (tuples)
+def generate_word_word_matrix(tokenized_lexica, isDebug=False):
+    """
+    Generate a Pykov matrix which calculates probabilities of [word, word] pairs
+    :param tokenized_lexica:
+    :return:
+    """
+    # create a list of bigrams
     all_bigrams = list(nltk.bigrams(tokenized_lexica))
 
-    # making a matrix which calculates probabilities of bigrams in text
+    # make a matrix which shows probabilities of bigrams in text
     cfdist = nltk.ConditionalFreqDist(all_bigrams)
-    matrix_word_word = pykov.Matrix() # essentially a dictionary with two keys and integer value
+    matrix_word_word = pykov.Matrix()  # essentially a dictionary with two keys and an integer value
     for bigram in all_bigrams:
         matrix_word_word[bigram[0], bigram[1]] = cfdist[bigram[0]][bigram[1]]
         #print("DEBUG", bigram, matrix_word_word[bigram[0], bigram[1]])
 
-    # setting a sum of each row equal to 1
-    stochastic(matrix_word_word) # overriden pykov function
+    # set a sum of each row equal to 1, matrix values will show probabilities, not frequencies
+    stochastic(matrix_word_word)  # overridden pykov function
 
-    print("bigrams probabilities test:", matrix_word_word.items())
+    if isDebug:
+        print("bigrams probabilities test:", matrix_word_word.items())
+
     return matrix_word_word
 
 
-def generate_word_pos_matrix(tagged_word_pairs):
-    # making a matrix which calculates probabilities of [word, tag] pairs
+def generate_word_pos_matrix(tagged_word_pairs, isDebug=False):
+    """
+    Generate a Pykov matrix which calculates probabilities of [word, tag] pairs
+    :param tagged_word_pairs:
+    :return:
+    """
     cfdist = nltk.ConditionalFreqDist(tagged_word_pairs)
-    #print cfdist[":"][":"]
     matrix_word_pos = pykov.Matrix()  # first - terminal, second - tag
 
     for pair in tagged_word_pairs:
         matrix_word_pos[pair[0], pair[1]] = cfdist[pair[0]][pair[1]]
 
-    stochastic(matrix_word_pos) # overriden
-    print("part-of-speech probabilities test:", matrix_word_pos.items())
+    stochastic(matrix_word_pos)
+
+    if isDebug:
+            print("part-of-speech probabilities test:", matrix_word_pos.items())
+
     return matrix_word_pos
 
 
-def generate_pos_pos_matrix(tagged_word_pairs):
-    # making a list of tag bigrams [tag, tag]
-    only_tags = [pair[1] for pair in tagged_word_pairs]  # we allow only tokenized_lexica here
+def generate_pos_pos_matrix(tagged_word_pairs, isDebug=False):
+    """
+    Generate a Pykov matrix which calculates probabilities of [tag, tag] pairs
+    :param tagged_word_pairs:
+    :return:
+    """
+    # make a list of tag bigrams [tag, tag]
+    only_tags = [pair[1] for pair in tagged_word_pairs]
     tag_bigrams = list(nltk.bigrams(only_tags))
 
-    # making a matrix which calculates probabilities of [tag, tag] pairs
+    # make a matrix which calculates probabilities of [tag, tag] pairs
     cfdist = nltk.ConditionalFreqDist(tag_bigrams)
     matrix_pos_pos = pykov.Matrix()
     for bigram in tag_bigrams:
         matrix_pos_pos[bigram[0], bigram[1]] = cfdist[bigram[0]][bigram[1]]
 
-    stochastic(matrix_pos_pos) # overriden
-    print("tags probabilities test:", matrix_pos_pos.items())
+    stochastic(matrix_pos_pos)  # overridden
+
+    if isDebug:
+        print("tags probabilities test:", matrix_pos_pos.items())
+
     return matrix_pos_pos
 
 
-def generate(tagged_words, matrices, length=5):  # tagged_words is a list of lists
+def generate(tagged_words, matrices, length=10):  # tagged_words is a list of lists
+    """
+    Continue the tagged_words list using probability matrices.
+    :param tagged_words: a pos-tagged and tokenized user input
+    :param matrices: 3 probability matrices
+    :param length: number of words which we are going to generate
+    :return: modified tagged_words with generates pairs added at the end of the list
+    """
     matrix_word_word = matrices[0]
     matrix_word_pos = matrices[1]
     matrix_pos_pos = matrices[2]
 
     while length > 0:
         length -= 1
-        word_n_tag = tagged_words[-1]
+        word_n_tag = tagged_words[-1]  # the content looks like ["i", "PRP"]
 
-        output_words = {} # key - word, value - probability
-        
-        # probability based on bigrams
-        #print("DEBUG", word_n_tag, matrix_word_word[word_n_tag[0]])
-        for pair in matrix_word_word.succ(word_n_tag[0]).items(): # [('A', 2), ('B', 6)]
-            """
-            if pair[0] == "END":
-                length = 0
-                break
-            if pair[0] == "START":
-                break;
-            """
+        next_word_probability = {}  # key - word, value - probability
+
+        # probability based on word-word bigrams
+        for pair in matrix_word_word.succ(word_n_tag[0]).items():  # [('A', 2), ('B', 6)]
             for tag_prob in matrix_word_pos.succ(pair[0]).items():
-                output_words[pair[0], tag_prob[0]] = pair[1] * tag_prob[1] + random.uniform(0.01, 0.1)
-                
-                
-        # probability based on matrix_pos_pos
-        for tag_prob in matrix_pos_pos.succ(word_n_tag[1]).items(): # [('A', 2), ('B', 6)]
+                next_word_probability[pair[0], tag_prob[0]] = pair[1] * tag_prob[1] + random.uniform(0.01, 0.1)
+
+        # probability based on pos-pos bigrams
+        for tag_prob in matrix_pos_pos.succ(word_n_tag[1]).items():  # [('A', 2), ('B', 6)]
             for word_prob in matrix_word_pos.pred(tag_prob[0]).items():
-                if (word_prob[0], tag_prob[0]) in output_words.keys():
-                    output_words[(word_prob[0], tag_prob[0])] += word_prob[1] * tag_prob[1]
+                if (word_prob[0], tag_prob[0]) in next_word_probability.keys():
+                    next_word_probability[(word_prob[0], tag_prob[0])] += word_prob[1] * tag_prob[1]
                 else:
-                    output_words[(word_prob[0], tag_prob[0])] = word_prob[1] * tag_prob[1]
+                    next_word_probability[(word_prob[0], tag_prob[0])] = word_prob[1] * tag_prob[1]
 
         # probability based on what word comes next
-        for output_pair in output_words.keys():
+        for output_pair in next_word_probability.keys():
             item = matrix_word_word.succ(word_n_tag[0]).items()
             #print("DEBUG", item)
             if list(item)[0] == output_pair[0]:
-                output_words[output_pair] += list(item)[1]
+                next_word_probability[output_pair] += list(item)[1]
             if len([pair[1] for pair in tagged_words if output_pair[1] == pair[1]]) > (length/2):
-                # penalty for repeating matrix_pos_pos
-                output_words[output_pair] -= 0.3
+                # penalty for repeating words
+                next_word_probability[output_pair] -= 0.3
             if output_pair in tagged_words:
-                output_words[output_pair] -= 0.2
-            
-                
-        chosen = max(output_words, key=lambda k: output_words[k])
-        tagged_words.append(chosen)
+                next_word_probability[output_pair] -= 0.2
+
+        tagged_words.append(max(next_word_probability, key=lambda k: next_word_probability[k]))
         """
-        for pair in output_words.items():
+        for pair in next_word_probability.items():
             if pair[-1] > 0.5:
                 print pair
                 print ""
@@ -172,30 +193,35 @@ def generate(tagged_words, matrices, length=5):  # tagged_words is a list of lis
     return tagged_words
 
 
-def start():
-    lexica = process_file(FILE_NAME)
+def start(file_name, user_sentence, length=10, is_debug=False):
+    """
+    Entry point.
+    :param file_name: name of the source text file
+    :param user_sentence: a starting sentence, string, provided by the user
+    :param length: how many words should we generate
+    :return:
+    """
+    lexica = process_file(file_name)
 
     tagged_word_pairs = nltk.pos_tag(lexica)
     # TODO: this line is inefficient, it seems to tag words without taking into account their context. However, might be fast.
 
-    probability_matrices = (generate_word_word_matrix(lexica),
-                            generate_word_pos_matrix(tagged_word_pairs),
-                            generate_pos_pos_matrix(tagged_word_pairs)
-    )
-
-    tokenized_user_input = nltk.word_tokenize(input("Write first word(s):").lower())
+    tokenized_user_input = nltk.word_tokenize(user_sentence)
+    user_input_pairs = nltk.pos_tag(tokenized_user_input)
 
     if (tokenized_user_input[-1]) not in lexica:
         return "Error! Please try a different word - the last word of your sentence is not present in the original text."
     # TODO: the last word of the text will raise an error in case it has no corresponding pair. Needs fixing. Maybe by adding an "END" token.
-    user_input_pairs = nltk.pos_tag(tokenized_user_input)
+
     try:
-        number_of_words = int(input("How many words should we generate? "))
+        number_of_words = int(length)
     except ValueError:
         return "Error! Please make sure to input a number!"
 
-    print("Input accepted.")
-
+    probability_matrices = (generate_word_word_matrix(lexica, is_debug),
+                            generate_word_pos_matrix(tagged_word_pairs, is_debug),
+                            generate_pos_pos_matrix(tagged_word_pairs, is_debug)
+                            )
 
     output = generate(user_input_pairs, probability_matrices, number_of_words)
 
@@ -205,10 +231,8 @@ def start():
 
 
 if __name__ == "__main__":
-    print(start())
-
-
-        
-            
-        
-
+    console_user_input = input("Enter the name of the source plain-text file: "), \
+        input("Write first word(s): ").lower(), \
+        input("How many words should we generate? ")
+    print("Input accepted.")
+    print(start(*console_user_input))
