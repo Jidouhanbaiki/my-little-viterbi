a546510827c68ddcfe474f566bf0b3e99c160b06
Jidouhanbaiki <ab2akiba@gmail.com>
2016-02-18 21:58:01 +0200

    jquery validation and tweaked the textarea a bit

--- web/MLV/viterbi_demo/forms.py
+++ web/MLV/viterbi_demo/forms.py
@@ -1,15 +1,17 @@
 from django import forms
 from .models import Text
 
-PHRASE_MY_CHOICE = "Use my own choice"
+PHRASE_MY_CHOICE = "Use my own text"
 
 
 class InputForm(forms.Form):
     titles = list(Text.objects.values_list('title'))
-    titles = [(PHRASE_MY_CHOICE, PHRASE_MY_CHOICE)] + [(item[0], item[0]) for item in titles]
-    source = forms.ChoiceField(choices=titles, initial=PHRASE_MY_CHOICE)
-    user_text = forms.CharField(widget=forms.Textarea, label="Input your own text", required=False)
-    sentence = forms.CharField(label="Start")
+    titles = [(item[0], item[0]) for item in titles] + [(PHRASE_MY_CHOICE, PHRASE_MY_CHOICE)]
+    source = forms.ChoiceField(choices=titles, initial=titles[0][0])
+    user_text = forms.CharField(widget=forms.Textarea(attrs={'rows': 10, 'cols': 80, 'style': 'resize: none'}),
+                                label="Input your own text",
+                                required=False)
+    sentence = forms.CharField(label="Start with (type a word or a phrase)")
     length = forms.IntegerField(min_value=1, max_value=200)
 
     def __init__(self, *args, **kwargs):
