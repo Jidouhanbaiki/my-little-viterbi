1ece99471e0d785fd8a2ec31b6eb62646b1350eb
Jidouhanbaiki <ab2akiba@gmail.com>
2016-02-20 16:49:26 +0200

    added ajax version. because I can

--- web/MLV/viterbi_demo/views.py
+++ web/MLV/viterbi_demo/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.template import loader
 from django.core.urlresolvers import reverse
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 
 
 import my_little_viterbi
@@ -11,7 +12,6 @@ from .models import Text
 
 
 def index(request):
-
     if request.method == 'POST':
         form = InputForm(request.POST)
         if form.is_valid():
@@ -27,12 +27,32 @@ def index(request):
                 length=form.cleaned_data['length'],
                 is_debug=False)
             messages.add_message(request, messages.INFO, output)
-            form = InputForm(request.POST)
-            #return render(request, 'viterbi_demo/index.html', {'output': output, 'form': form})
             return HttpResponseRedirect(reverse('index'))
-
     else:
         form = InputForm()
     return render(request, 'viterbi_demo/index.html', {'form': form})
+
+
+@csrf_exempt
+def ajax(request):
+    if request.is_ajax():
+        form = InputForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data['source'] == PHRASE_MY_CHOICE:
+                raw_text = form.cleaned_data['user_text']
+            else:
+                t = Text.objects.get(title__exact=form.cleaned_data['source'])
+                raw_text = t.content
+                form.cleaned_data['user_text'] = ""
+            output = my_little_viterbi.start(
+                raw_text=raw_text,
+                user_sentence=form.cleaned_data['sentence'],
+                length=form.cleaned_data['length'],
+                is_debug=False)
+            return JsonResponse({"data": output})
+    else:
+        form = InputForm()
+    return render(request, 'viterbi_demo/index_ajax.html', {'form': form})
+
 
 
